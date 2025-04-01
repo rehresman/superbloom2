b09e7bc77fc20bc0cfaf5d0f94083e637774ce2c
aaron <37132756+motherbrainn@users.noreply.github.com>
2025-04-01 09:12:55 -0700

    Feat/initial UI (#1)
    * feat: initial vite project
    
    * feat: add react piano

---
+++ superbloom2-client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { OnScreenPiano } from "./components/OnScreenPiano";
+import SynthPiano from "./components/ReactPiano";
+
+function App() {
+  return (
+    <>
+      {/* <OnScreenPiano /> */}
+      <SynthPiano />
+    </>
+  );
+}
+
+export default App;
